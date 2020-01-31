ae8ab06f637527ab15a23818602bfef558fb6e01
Sharon Lee <sharonlee714@gmail.com>
2020-01-31 10:16:55 -0800

    add files and README

---
+++ UncommonShelter/Models/Animal.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace UncommonShelter
+{
+    public class Animal
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
